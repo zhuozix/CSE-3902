b78f0e2cc6c8c864f72bbf17550162bbf676dc0c
sethw216 <90290232+sethw216@users.noreply.github.com>
2023-02-06 11:09:58 -0500

    Seth item sprite commit 2

--- Sprint0/Content/Command.cs
+++ Sprint0/Content/Command.cs
@@ -57,4 +57,51 @@ namespace Sprint0.Content
 	}
     //
 
+    // Seth Sprint 2
+
+    public class increaseItemIndex : ICommand
+    {
+        private Game1 game;
+
+        public increaseItemIndex(Game1 game)
+        {
+            this.game = game;
+        }
+
+        public void Execute()
+        {
+            if (game.DisplayItem < 4)
+            {
+                game.DisplayItem = game.DisplayItem + 1;
+            } else
+            {
+                game.DisplayItem = 0;
+            }
+            
+        }
+    }
+
+    public class decreaseItemIndex : ICommand
+    {
+        private Game1 game;
+
+        public decreaseItemIndex(Game1 game)
+        {
+            this.game = game;
+        }
+
+        public void Execute()
+        {
+            if (game.DisplayItem > 0)
+            {
+                game.DisplayItem = game.DisplayItem - 1;
+            }
+            else
+            {
+                game.DisplayItem = 4;
+            }
+
+        }
+    }
+
 }
