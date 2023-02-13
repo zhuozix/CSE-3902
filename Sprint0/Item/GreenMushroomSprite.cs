2c2b8c549956090f56e23e62ccaefe4da5fe3f7d
sethw216 <90290232+sethw216@users.noreply.github.com>
2023-02-13 12:42:19 -0500

    Changed movement for star and mushrooms

--- Sprint0/Item/GreenMushroomSprite.cs
+++ Sprint0/Item/GreenMushroomSprite.cs
@@ -9,10 +9,49 @@ using System.Threading.Tasks;
 
 namespace Sprint0.Item
 {
-    internal class GreenMushroomSprite : NoneMovingAnimatedSprite
+    internal class GreenMushroomSprite : MovingNoneAnimatedSprite
     {
-        public GreenMushroomSprite(Texture2D texture, Vector2 position, int rows, int cols)
-            : base(texture, position, rows, cols)
-        { }
+
+        private GraphicsDeviceManager graphics;
+        private Vector2 originalPosition;
+        //1 means right and down, -1 means move left and up
+        private int moveDirection;
+        static float moveSpeed = 100f;
+        private int xDirection = 1;
+        public GreenMushroomSprite(Texture2D texture, Vector2 position, int rows, int cols, GraphicsDeviceManager graphics, int moveDirection)
+            : base(texture, position, rows, cols, graphics, moveDirection)
+        {
+            this.graphics = graphics;
+            this.originalPosition = position;
+            this.moveDirection = moveDirection;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            float currHeight = position.Y;
+            float currWidth = position.X;
+            float maxHeight = graphics.PreferredBackBufferHeight - (Texture.Height+1);
+            float maxWidth = graphics.PreferredBackBufferWidth - (Texture.Width + 1);
+            float minHeight = 0;
+            float minWidth = 0;
+            if (currHeight >= maxHeight)
+            {
+                position.Y = maxHeight;
+            }
+            
+            if (Position.X >= maxWidth && xDirection > 0)
+            {
+                position.X = maxWidth - 1;
+                xDirection = -xDirection;
+            }
+            else if (Position.X <= minWidth && xDirection < 0)
+            {
+                xDirection = -xDirection;
+            }
+
+
+            position += new Vector2(moveSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds * xDirection, moveSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds * moveDirection);
+            
+        }
     }
 }
--- Sprint0/Item/StarSprite.cs
+++ Sprint0/Item/StarSprite.cs
@@ -9,10 +9,63 @@ using System.Threading.Tasks;
 
 namespace Sprint0.Item
 {
-    internal class StarSprite : NoneMovingAnimatedSprite
+    internal class StarSprite : MovingAnimatedSprite
     {
-        public StarSprite(Texture2D texture, Vector2 position, int rows, int cols)
-            : base(texture, position, rows, cols)
-        { }
+        private GraphicsDeviceManager graphics;
+        private Vector2 originalPosition;
+        //1 means right and down, -1 means move left and up
+        private int moveDirection;
+        static float moveSpeed = 100f;
+        private int xDirection = 1;
+        public StarSprite(Texture2D texture, Vector2 position, int rows, int cols, GraphicsDeviceManager graphics, int moveDirection)
+            : base(texture, position, rows, cols, graphics, moveDirection)
+        {
+            this.graphics = graphics;
+            this.originalPosition = position;
+            this.moveDirection = moveDirection;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            float currHeight = position.Y;
+            float currWidth = position.X;
+            float maxHeight = graphics.PreferredBackBufferHeight - (Texture.Height + 1);
+            float maxWidth = graphics.PreferredBackBufferWidth - (Texture.Width / this.totalFrames + 1);
+            float minHeight = graphics.PreferredBackBufferHeight - 50;
+            float minWidth = 0;
+
+            timeSinceLastFrameTransition += gameTime.ElapsedGameTime.TotalSeconds;
+            if (timeSinceLastFrameTransition > animateFrequency)
+            {
+                timeSinceLastFrameTransition = 0;
+                currentFrame++;
+                if (currentFrame == totalFrames)
+                {
+                    currentFrame = 0;
+                }
+            }
+
+            if (currHeight >= maxHeight)
+            {
+                moveDirection = -moveDirection;
+            }
+            else if (currHeight <= minHeight && moveDirection < 0)
+            {
+                moveDirection = -moveDirection;
+            }
+
+            if (Position.X >= maxWidth && xDirection > 0)
+            {
+                xDirection = -xDirection;
+            }
+            else if (Position.X <= minWidth && xDirection < 0)
+            {
+                xDirection = -xDirection;
+            }
+
+
+            position += new Vector2(moveSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds * xDirection, moveSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds * moveDirection);
+
+        }
     }
 }
