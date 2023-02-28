ea2ffed9dfa6d69f7e8c05b6fc597a6309fb54af
sethw216 <90290232+sethw216@users.noreply.github.com>
2023-02-27 20:39:45 -0500

    Worked on level loader

---
+++ AnimatedBlockSprite.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Sprint0.Sprites;
+
+namespace Sprint0.Blocks
+{
+	internal class AnimatedBlockSprite : ISprite
+	{
+		public Texture2D Texture { get; set; }
+		public int Rows { get; set; }
+		public int Columns { get; set; }
+		public Vector2 Position { get; set; }
+
+		public int Width => throw new NotImplementedException();
+
+		public int Height => throw new NotImplementedException();
+
+		private int currentFrame;
+		private int totalFrames;
+		private Vector2 location;
+		internal double timeSinceLastFrameTransition = 0.0;
+		public static double animateFrequency = 0.9 / 5.0;
+
+		public AnimatedBlockSprite(Texture2D texture, int rows, int columns, Vector2 l)
+		{
+			Texture = texture;
+			Rows = rows;
+			Columns = columns;
+			currentFrame = 0;
+			totalFrames = Rows * Columns;
+			location = l;
+		}
+		public void Update(GameTime gameTime)
+		{
+			timeSinceLastFrameTransition += gameTime.ElapsedGameTime.TotalSeconds;
+			if (timeSinceLastFrameTransition > animateFrequency)
+			{
+				timeSinceLastFrameTransition = 0;
+				currentFrame++;
+				if (currentFrame == totalFrames)
+				{
+					currentFrame = 0;
+				}
+			}
+		}
+
+		public void Draw(SpriteBatch spriteBatch, bool isFlipped)
+		{
+			int width = Texture.Width / Columns;
+			int height = Texture.Height / Rows;
+			int row = currentFrame / Columns;
+			int column = currentFrame % Columns;
+
+			Rectangle sourceRectangle = new Rectangle(width * column, height * row, width, height);
+			Rectangle destinationRectangle = new Rectangle((int)location.X - width, (int)location.Y - height, width, height);
+
+			spriteBatch.Draw(Texture, destinationRectangle, sourceRectangle, Color.White);
+			
+		}
+	}
+}
+
---
+++ Game1.cs
@@ -0,0 +1,178 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Sprint0.Content;
+using Sprint0.Sprites;
+using System.Collections;
+using Sprint0.Enemy;
+using Sprint0.Factory;
+using Sprint0.MarioPlayer;
+using Sprint0.Sprites.Lists;
+using Sprint0.ObjectManager;
+using Sprint0.LevelLoader;
+
+namespace Sprint0
+{
+    public class Game1 : Game
+    {
+        private static Game1 instance;
+        public static Game1 Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+        public GraphicsDeviceManager _graphics;
+        public SpriteBatch _spriteBatch;
+        /*
+         * Controllers
+         */
+        private IController keyboardController;
+
+        /*
+         * SpriteFactories
+         */
+        public SpritesFactory spritesFactory;
+
+        /*
+         * Current Sprites
+         */
+        //Mario
+        public Mario mario;
+        //Npc
+        public ISprite currentEnemy;
+        public ISprite currentBlock;
+        public ISprite currentItem;
+
+        /*
+         *  Sprites Lists
+         */
+        private ArrayList blockList;
+        private ArrayList itemList;
+        private ArrayList enemyList;
+        public FireBallList fireBallList;
+
+        /*
+         * Command Control
+         */
+        public int DisplaySprite { get; set; }
+        public int DisplayBlock { get; set; }
+        public int DisplayItem { get; set; }
+        public int DisplayEnemy { get; set; }
+
+        public GameObjectManager gameObjectManager;
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            //Factories
+            spritesFactory = new SpritesFactory(this);
+            spritesFactory.initalize();
+
+
+            //controllers
+            keyboardController = new KeyboardController(this);
+
+
+            //Lists
+            gameObjectManager = new GameObjectManager();
+
+            blockList = new ArrayList();
+            itemList = new ArrayList();
+            enemyList = new ArrayList();
+            fireBallList = new FireBallList();
+
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {         
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+
+            /*
+             * Load Sprites 
+             */
+
+            //Player
+            mario = new Mario(new Vector2(GraphicsDevice.Viewport.Width / 2, GraphicsDevice.Viewport.Height / 2), this);
+            // NPC lists
+            spritesFactory.addAllBlocks(blockList);
+            spritesFactory.addAllItems(itemList);
+            spritesFactory.addAllEnemies(enemyList);
+
+            foreach (ISprite obj in blockList)
+            {
+                gameObjectManager.addObject(obj, "block");
+            }
+            foreach (ISprite obj in enemyList)
+            {
+                gameObjectManager.addObject(obj, "enemy");
+            }
+            foreach (ISprite obj in itemList)
+            {
+                gameObjectManager.addObject((ISprite)obj, "item");
+            }
+            gameObjectManager.addObject(mario, "player");
+            //Load commands to controller
+            keyboardController.loadCommonCommand();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            
+
+            //display the sprite from the sprite list one at a time.
+            //currentEnemy = (ISprite)enemyList[DisplayEnemy];
+            //currentBlock = (ISprite)blockList[DisplayBlock];
+            //currentItem = (ISprite)itemList[DisplayItem];
+            
+            keyboardController.UpdateInput();
+            //Players
+            //mario.Update(gameTime);
+            
+            //Blocks
+            //currentBlock.Update(gameTime);
+            gameObjectManager.update(gameTime);
+			
+            //Items
+            //currentItem.Update(gameTime);
+            
+            //Enemies
+            //currentEnemy.Update(gameTime);
+
+            //Fireballs
+            //fireBallList.Update(gameTime);
+
+            base.Update(gameTime);
+           
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            _spriteBatch.Begin();
+
+            //mario.Draw(_spriteBatch, true);
+            //currentBlock.Draw(_spriteBatch, true);
+            gameObjectManager.Draw(_spriteBatch, true);
+            //currentItem.Draw(_spriteBatch, true);
+            //currentEnemy.Draw(_spriteBatch, true);
+            //fireBallList.Draw(_spriteBatch);
+
+
+            _spriteBatch.End();
+        } 
+
+        public void GameReset()
+        {
+            this.Initialize();
+        }
+    }
+}
