19677c9fbbd5727123a62df1ddc86a258117c5af
Yao Lu <adobelystan@gmail.com>
2023-03-05 15:02:31 -0500

    add interface for blockstate and itemstate

---
+++ Sprint0/Blocks/State/IBlockState.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint0.Blocks.State
+{
+    public interface IBlockState
+    {
+        void Enter(IBlockState previousState);
+        void Exit();
+
+        void DefaultTransition();
+        void BumpTransition();
+        void ShatterTransition();
+        void UsedTransition();
+
+        void Update(GameTime gameTime);
+    }
+}
---
+++ Sprint0/Item/State/IItemState.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Sprint0.EntityClasses.Item.State
+{
+    public interface IItemState
+    {
+        void Enter(IItemState previousState);
+        void Exit();
+
+        void DispenseTransition();
+        void IdleTransition();
+        void MoveTransition();
+        void BounceTransition();
+
+        void Update(GameTime gameTime);
+    }
+}
