65dc062a35d84a49f91f28e8cae5d1b7fa17f1a5
sethw216 <90290232+sethw216@users.noreply.github.com>
2023-03-10 10:33:32 -0500

    Removed code related to sprint two and display item/enemy/block

--- Sprint0/Command/GameControlCMD/Reset.cs
+++ Sprint0/Command/GameControlCMD/Reset.cs
@@ -16,9 +16,6 @@ namespace Sprint0.Command.GameControlCMD
         }
         public void Execute()
         {
-            this.game.DisplayBlock = 0;
-            this.game.DisplayItem = 0;
-            this.game.DisplayEnemy = 0;
             game.GameReset();
         }
     }
